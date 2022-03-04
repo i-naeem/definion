23440f5cd80843a807b0bd0e25d02ceef366596e
Eva <eva@gmail.com>
2022-03-04 21:10:55 +0500

    :sparkles: add search form on home page

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,3 +1,20 @@
+import { Container, Form, Button, InputGroup } from 'react-bootstrap';
+
 export const Home = (props) => {
-  return <h1>Home</h1>;
+  return (
+    <Container className="p-5">
+      <section>
+        <h1>Definion</h1>
+        <p>A simple dictionary to see the defination of a word.</p>
+      </section>
+      <Form>
+        <InputGroup>
+          <Form.Control placeholder="Search" size="lg" />
+          <Button type="submit" size="lg">
+            Define
+          </Button>
+        </InputGroup>
+      </Form>
+    </Container>
+  );
 };
