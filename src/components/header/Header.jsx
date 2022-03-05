bd5b15611ec1b30753015ec5e3ee9f31c992afa4
Eva <eva@gmail.com>
2022-03-05 11:48:29 +0500

    :fire: make brand clickable

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,14 @@
 import { Navbar, NavbarBrand, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 export const Header = (props) => {
   return (
     <header>
       <Navbar variant="light" bg="light">
         <Container>
-          <NavbarBrand>Definion</NavbarBrand>
+          <NavbarBrand as={Link} to="/">
+            Definion
+          </NavbarBrand>
         </Container>
       </Navbar>
     </header>
